62e78c05ec898ce805402cf8bf45efc1eabd9137
jhk5263 <71970354+jhk5263@users.noreply.github.com>
2025-08-17 23:08:54 +0800

    更新 dl.js

--- dl.js
+++ dl.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         视频下载器(test)
 // @namespace    http://tampermonkey.net/
-// @version      1.27
+// @version      1.271
 // @description  在 TopMediAI 的文本转视频页面，当视频生成后自动添加一个下载按钮。
 // @author       haway
 // @match        *.topmediai.com/*
